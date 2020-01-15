adb73b6fc63e58d11e4bc658430e13cbb9393464
wreulicke <masaya2dies@gmail.com>
2020-01-16 00:58:15 +0900

    init

--- http-timeouts/main.go
+++ http-timeouts/main.go
@@ -1,12 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	m := http.NewServeMux()
+	m.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World"))
 	})
-	http.ListenAndServe(":8080", nil)
-
+	srv := &http.Server{
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		Handler:      m,
+		Addr:         ":8080",
+	}
+	log.Println(srv.ListenAndServe())
 }

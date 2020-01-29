0552bbd5034f8bcc09b6355f3dad9a114be41f89
wreulicke <masaya2dies@gmail.com>
2020-01-30 04:12:29 +0900

    implement string and test

--- go-interpreter/monkey/ast/ast.go
+++ go-interpreter/monkey/ast/ast.go
@@ -2,6 +2,8 @@ package ast
 
 import "github.com/wreulicke/go-sandbox/go-interpreter/monkey/token"
 
+import "bytes"
+
 type Program struct {
 	Statements []Statement
 }
@@ -11,6 +13,14 @@ func (p *Program) TokenLiteral() string {
 		return p.Statements[0].TokenLiteral()
 	}
 	return ""
+}
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
 }
 
 type statement struct{}
@@ -32,14 +42,56 @@ func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
 
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral())
+	out.WriteRune(' ')
+	out.WriteString(ls.Name.String())
+
+	out.WriteString(" = ")
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
 type ReturnStatement struct {
 	statement
 	Token       token.Token
 	ReturnValue Expression
 }
 
-func (r *ReturnStatement) TokenLiteral() string {
-	return r.Token.Literal
+func (rs *ReturnStatement) TokenLiteral() string {
+	return rs.Token.Literal
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral())
+	out.WriteRune(' ')
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	statement
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
 }
 
 type Identifier struct {
@@ -52,8 +104,13 @@ func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
 
+func (i *Identifier) String() string {
+	return i.Value
+}
+
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {

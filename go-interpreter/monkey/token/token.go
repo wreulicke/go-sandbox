31670292728c973f2d686597a782e5f2ff717e91
wreulicke <masaya2dies@gmail.com>
2020-01-29 19:52:04 +0900

    init

---
+++ go-interpreter/monkey/token/token.go
@@ -0,0 +1,63 @@
+package token
+
+type TokenType int
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = iota
+	EOF
+
+	IDENT
+	INT
+
+	ASSIGN
+	PLUS
+	MINUS
+	BANG
+	ASTERISK
+	SLASH
+
+	EQ
+	NOT_EQ
+
+	LT
+	GT
+
+	COMMA
+	SEMICOLON
+
+	LPAREN
+	RPAREN
+
+	LBRACE
+	RBRACE
+
+	FUNCTION
+	LET
+	RETURN
+	TRUE
+	FALSE
+	IF
+	ELSE
+)
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"return": RETURN,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}

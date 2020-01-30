5f1a2b65b3e081d16aded0e1e96af3c466b2ca7f
wreulicke <masaya2dies@gmail.com>
2020-01-31 08:06:19 +0900

    add repl

--- go-interpreter/monkey/cli/cli.go
+++ go-interpreter/monkey/cli/cli.go
@@ -4,22 +4,34 @@ import (
 	"github.com/spf13/cobra"
 
 	lexerRepl "github.com/wreulicke/go-sandbox/go-interpreter/monkey/lexer/repl"
+	parserRepl "github.com/wreulicke/go-sandbox/go-interpreter/monkey/parser/repl"
 )
 
 func New() *cobra.Command {
 	c := &cobra.Command{
 		Use: "monkey",
 	}
-	c.AddCommand(NewLexerCommand())
+	c.AddCommand(NewLexerCommand(), NewParserCommand())
 	return c
 }
 
 func NewLexerCommand() *cobra.Command {
 	c := &cobra.Command{
 		Use:   "lexer",
-		Short: "s",
+		Short: "l",
 		Run: func(cmd *cobra.Command, args []string) {
 			lexerRepl.Start()
+		},
+	}
+	return c
+}
+
+func NewParserCommand() *cobra.Command {
+	c := &cobra.Command{
+		Use:   "parser",
+		Short: "p",
+		Run: func(cmd *cobra.Command, args []string) {
+			parserRepl.Start()
 		},
 	}
 	return c
